e9970685b1b6c78e3c67c395bb033bbd7a21045b
Duane Mitchell <duane.a.mitchell@outlook.com>
2026-05-24 09:55:12 -0400

    updated event quote

--- Models/QuoteFields.cs
+++ Models/QuoteFields.cs
@@ -6,7 +6,8 @@ public interface IQuoteField
     string Key   { get; }
 }
 
-public record TextField    (string Label, string Key)                       : IQuoteField;
-public record NumberField  (string Label, string Key)                       : IQuoteField;
-public record TextareaField(string Label, string Key)                       : IQuoteField;
-public record DropdownField(string Label, string Key, List<string> Options) : IQuoteField;
+public record TextField    (string Label, string Key)                                : IQuoteField;
+public record NumberField  (string Label, string Key)                                : IQuoteField;
+public record TextareaField(string Label, string Key)                                : IQuoteField;
+public record DropdownField(string Label, string Key, List<string> Options)          : IQuoteField;
+public record Info         (string Label, string Key, string AutoComplete, string Type)      : IQuoteField;
--- obj/Debug/net8.0/MitchellFabrication.AssemblyInfo.cs
+++ obj/Debug/net8.0/MitchellFabrication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MitchellFabrication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4701e22fa2f4d36711a01c28b08b2133494d9b7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86b2bdf334857e43ba9bbaf12e13f0199b9274d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MitchellFabrication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MitchellFabrication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
